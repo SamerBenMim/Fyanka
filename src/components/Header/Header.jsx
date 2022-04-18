14414b797f6d8ca9f2fa8c713149e71cb0bfebab
SamerBenMim <samerbenmim@insat.u-carthage.tn>
2022-04-18 16:30:18 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import classes from "./Header.module.css";
+import logo from "../../assets/fireworks.png"
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <img src={logo} alt="img" />
+      <h6>FYANKA</h6>
+    </div>
+  );
+};
+
+export default Header;
+<div></div>;
